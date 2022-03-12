8d3e1f3a1a097d8e92875d257e4fbe9d8ec20c83
Juan Jerrah <juanjerrah15@gmail.com>
2022-03-12 10:40:18 -0300

    Post para adicionar produtos implementado

---
+++ AvonaleApiVendas/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using AvonaleApiVendas.Data;
+using AvonaleApiVendas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvonaleApiVendas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> InsertProduct(Product product)
+        {
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Product.ToListAsync());
+
+        }
+    }
+}
--- AvonaleApiVendas/Models/Product.cs
+++ AvonaleApiVendas/Models/Product.cs
@@ -14,12 +14,11 @@ namespace AvonaleApiVendas.Models
         public decimal ProductPrice { get; set; }
 
         [Required(ErrorMessage = "Campo obrigatório!")]
-        [MinLength(1, ErrorMessage = "Precisa de pelo menos 1 produto em estoque!")]
+        
         public int QuantityStock { get; set; }
 
         public DateTime LastSaleTime { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório!")]
         public decimal LastSalePrice { get; set; }
     }
 }
