83e367178bd0f4407e005a52637c4c8d8df93668
Juan Jerrah <juanjerrah15@gmail.com>
2022-03-13 14:14:36 -0300

    Lógica de pagamentos implementada

---
+++ AvonaleApiVendas/Controllers/PaymentsController.cs
@@ -0,0 +1,19 @@
+﻿using AvonaleApiVendas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvonaleApiVendas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        [HttpPost]
+        public Payment Pay(Purchase purchase)
+        {
+            return (purchase.PurchasePrice <= 100) ? 
+                new Payment(purchase.PurchasePrice, "Reprovado!") : 
+                    new Payment(purchase.PurchasePrice, "Aprovado!");
+        }
+    }
+}
