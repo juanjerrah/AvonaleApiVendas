5a51bd755b984c6fdd25b466ac755691653c8da2
Juan Jerrah <juanjerrah15@gmail.com>
2022-03-13 13:48:49 -0300

    Controller

--- AvonaleApiVendas/Controllers/ProductsController.cs
+++ AvonaleApiVendas/Controllers/ProductsController.cs
@@ -17,10 +17,70 @@ namespace AvonaleApiVendas.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> AllProducts()
+        {
+            try
+            {
+               return Ok(await _context.Product.ToListAsync());
+            }catch{
+                return BadRequest();
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> ProductById(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if(product == null)
+            {
+                return BadRequest("Produto não encontrado!");
+            }
+            return Ok(product);
+            
+        }
+
         [HttpPost]
-        public async Task<ActionResult<Product>> InsertProduct(Product product)
+        public async Task<ActionResult<List<Product>>> InsertProduct(Product product)
         {
             _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Product.ToListAsync());
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Product>>> UpdateProduct(Product product)
+        {
+            var products = await _context.Product.FindAsync(product.Id);
+            if(products == null)
+            {
+                return BadRequest("Produtonão encontrado!");
+            }
+
+            products.ProductName = product.ProductName;
+            products.ProductPrice = product.ProductPrice;
+            products.QuantityStock = product.QuantityStock;
+            products.LastSalePrice = product.LastSalePrice;
+            products.LastSaleTime = DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Product.ToListAsync());
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<List<Product>>> DeleteProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if(product == null)
+            {
+                return BadRequest("Produto não encontrado!");
+            }
+
+            _context.Product.Remove(product);
             await _context.SaveChangesAsync();
 
             return Ok(await _context.Product.ToListAsync());
---
+++ AvonaleApiVendas/Controllers/PurchasesController.cs
@@ -0,0 +1,16 @@
+﻿using AvonaleApiVendas.Data;
+
+namespace AvonaleApiVendas.Controllers
+{
+    public class PurchasesController
+    {
+        private readonly DataContext _context;
+
+        public PurchasesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
